0a78ddc48f52ea8cdfdd1180874b3ccd13c30f85
elvee <lowie@beecave.nl>
2024-12-07 15:32:08 +0100

    feat ✨: Add Dutch filth classes for location, organization, and name detection.

---
+++ custom_detectors/dutch_filth.py
@@ -0,0 +1,10 @@
+from scrubadub.filth import Filth
+
+class LocationFilth(Filth):
+    type = 'location'
+
+class OrganizationFilth(Filth):
+    type = 'organization'
+
+class NameFilth(Filth):
+    type = 'name' 
